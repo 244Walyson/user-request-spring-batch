c205800705996fc6e0af2b5a72d652e40f95897a
walyson <walysonmoises1342@gmail.com>
2023-08-03 00:51:33 -0300

    project created

---
+++ src/test/java/com/waly/userrequestspringbatch/UserRequestSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waly.userrequestspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
